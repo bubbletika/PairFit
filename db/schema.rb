5c797cb9e40e281d9eb465f9d4b0fdebf77e67fc
Andrew Simon <asimon@ucsd.edu>
2015-02-08 15:49:06 -0800

    Finish log in / log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208205703) do
+ActiveRecord::Schema.define(version: 20150208232309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150208205703) do
     t.string   "email"
     t.string   "name"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
